2ee4124a6cc6fc10069191cd70176fa214440be5
Ilia Atanov <chip0@mail.ru>
2019-10-14 18:42:40 +0500

    Добавил поле ввода заявки

--- src/views/Terminal.vue
+++ src/views/Terminal.vue
@@ -1,6 +1,5 @@
 <template>
-  <v-container fluid>
-     <v-row>
+  <v-container>
     <v-row>
       <v-col cols="4">
     
@@ -26,25 +25,79 @@
         </v-simple-table>
 
       </v-col>
+
+      <v-col cols="4">
+
+        <v-form ref="form" lazy-validation>
+          <v-container>
+            <v-row dense>
+              <v-col>
+                <v-text-field dense disabled label="Bid" outline />
+              </v-col>
+              <v-col>
+                <v-text-field dense disabled label="Ask" outline />
+              </v-col>
+            </v-row>
+
+            <v-row dense>
+              <v-col>
+                <v-select dense :items="['Limit','Market']" value="Limit">
+
+                </v-select>
+              </v-col>            
+              <v-col>
+                <v-select dense :items="['Day','GTC']" value="Day"></v-select>
+              </v-col>            
+            </v-row>
+
+            <v-row dense>
+              <v-col>
+                <v-text-field dense v-model="amount" label="Amount" required></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field dense v-model="price" label="Price" required></v-text-field>
+              </v-col>
+            </v-row>
+
+            <v-row no-gutters>
+              <v-col>
+                <v-btn small @click="handlerBuy" block color="success">Buy</v-btn>
+              </v-col>
+              <v-col>
+                <v-btn small @click="handlerSell" block color="error">Sell</v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
+
+      </v-col>
+
     </v-row>
 
-    <v-row align="stretch">
-      <v-col>
+    <v-row>
+      <v-col cols="6">
         <v-tabs height="35">
           <v-tab>Positions</v-tab>
           <v-tab>Orders</v-tab>
           <v-tab>Trades</v-tab>
           
           <v-tab-item transition="none" reverse-transition="none">
-            <v-data-table dense :headers="position_headers" :items="positions" item-key="ticker"  fixed-header disable-pagination hide-default-footer></v-data-table>
+            <v-data-table dense height="300" :headers="position_headers" :items="positions" item-key="id"  fixed-header disable-pagination hide-default-footer></v-data-table>
           </v-tab-item>
 
           <v-tab-item transition="none" reverse-transition="none">
-            Orders
+            <v-data-table dense :headers="order_headers" :items="orders" item-key="id">
+              <template v-slot:item.state="{ item }">
+                <v-chip color="green" dark label x-small>{{ item.state }}</v-chip>
+<!--                 <div class="green text-center">
+                  <span class="white--text">{{ item.state }}</span>
+                </div>
+ -->              </template>
+            </v-data-table>
           </v-tab-item>
 
           <v-tab-item transition="none"  reverse-transition="none">
-            <v-data-table dense :headers="position_headers" :items="positions" item-key="ticker" class="elevation-1"></v-data-table>
+            <v-data-table dense :headers="position_headers" :items="positions" item-key="id"></v-data-table>
           </v-tab-item>
 
 
@@ -52,7 +105,6 @@
         </v-tabs>      
       </v-col>
     </v-row>
-     </v-row>
   </v-container>
 </template>
 
@@ -60,7 +112,43 @@
 <script lang="ts">
 export default {
   data() {
+    // Generate positions
+    const ipos = [];
+    const tickers: string[] = ['AAPL.NASDAQ', 'AMZN.NASDAQ', 'GOOG.NASDAQ', 'EUR/USD.E.FX', 'USD/JPY.E.FX', 'GBP/USD.E.FX'];
+
+    for (let i = 0; i < 100; ++i) {
+      ipos[i] = {
+          id: i,
+          ticker: tickers[Math.floor(Math.random() * tickers.length)],
+          position: Math.floor(Math.random() * 100) + 1,
+          avgprice: (Math.random() * 100 + 10).toFixed(2),
+          price: (Math.random() * 100 + 10).toFixed(2),
+          pnl: (Math.random() * 100 + 10).toFixed(2),
+      };
+    }
+
     return {
+      order_headers: [
+        {text: 'State', value: 'state'},
+        {text: 'Ticker', value: 'ticker'},
+        {text: 'Type', value: 'type'},
+        {text: 'Side', value: 'side'},
+        {text: 'Quantity', value: 'quantity'},
+        {text: 'Time', value: 'time'},
+      ],
+
+      orders: [
+        {
+          id: '0001',
+          state: 'open',
+          ticker: 'EUR/USD.E.FX',
+          type: 'limit',
+          side: 'buy',
+          quantity: 10,
+          time: new Date(2017, 4, 4, 17, 23, 42, 11).toLocaleString(),
+        },
+      ],
+
       position_headers: [
         {text: 'Ticker', value: 'ticker'},
         {text: 'Position', value: 'position'},
@@ -69,246 +157,7 @@ export default {
         {text: 'P&L', value: 'pnl'},
       ],
 
-      positions: [
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-        {
-          ticker: 'AAPL.NASDAQ',
-          position: 50,
-          avgprice: 210,
-          price: 233,
-          pnl: 100,
-        },
-      ],
+      positions: ipos,
 
       symbols: [
         {
