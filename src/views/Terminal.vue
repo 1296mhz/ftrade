f6f704c1ee81031696c6239fe7c09ae1161485bb
Dmitry Ziryanov <cshlovjah@gmail.com>
2019-10-21 11:00:07 +0500

    Отзывчивость и кард

--- src/views/Terminal.vue
+++ src/views/Terminal.vue
@@ -1,123 +1,85 @@
 <template>
-  <v-container>
+  <v-container fluid>
     <v-row>
-      <v-col cols="10">
+      <v-col cols="12" md="12" xs="12" sm="12" lg="12" xl="12">
         <v-row>
-          <v-col cols="6">
-            <v-simple-table dense>
-              <template v-slot:default>
-                <thead>
-                  <tr>
-                    <th>Ticker</th>
-                    <th>Bid</th>
-                    <th>Ask</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="symbol in symbols" :key="symbol.ticker">
-                    <td>
-                      <v-icon small :color="symbol.color">mdi-circle</v-icon>
-                      {{ symbol.ticker }}
-                    </td>
-                    <td>{{ symbol.bid }}</td>
-                    <td>{{ symbol.ask }}</td>
-                    <td>
-                      <v-icon small @click="deleteItem(symbol)">mdi-close-box-outline</v-icon>
-                    </td>
-                  </tr>
-                </tbody>
-              </template>
-            </v-simple-table>
+          <v-col cols="12" xs="12" sm="12" md="6" lg="6" xl="6">
+            <v-card height="100%">
+              <v-simple-table dense >
+                <template v-slot:default>
+                  <thead>
+                    <tr>
+                      <th>Ticker</th>
+                      <th>Bid</th>
+                      <th>Ask</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="symbol in symbols" :key="symbol.ticker">
+                      <td>
+                        <v-icon small :color="symbol.color">mdi-circle</v-icon>
+                        {{ symbol.ticker }}
+                      </td>
+                      <td>{{ symbol.bid }}</td>
+                      <td>{{ symbol.ask }}</td>
+                      <td>
+                        <v-icon small @click="deleteItem(symbol)">mdi-close-box-outline</v-icon>
+                      </td>
+                    </tr>
+                  </tbody>
+                </template>
+              </v-simple-table>
+            </v-card>
           </v-col>
 
-          <v-col cols="6">
-            <highcharts class="stock" :constructor-type="'stockChart'" :options="stockOptions"></highcharts>
+          <v-col cols="12" xs="12" sm="12" md="6" lg="6" xl="6">
+            <v-card height="100%">
+              <highcharts class="stock" :constructor-type="'stockChart'" :options="stockOptions"></highcharts>
+            </v-card>
           </v-col>
         </v-row>
 
         <v-row>
-          <v-col cols="12">
-            <v-tabs height="35">
-              <v-tab>Positions</v-tab>
-              <v-tab>Orders</v-tab>
-              <v-tab>Trades</v-tab>
+          <v-col cols="12" md="12" xs="12" sm="12" lg="12" xl="12">
+            <v-card height="100%">
+              <v-tabs height="35">
+                <v-tab>Positions</v-tab>
+                <v-tab>Orders</v-tab>
+                <v-tab>Trades</v-tab>
 
-              <v-tab-item transition="none" reverse-transition="none">
-                <v-data-table
-                  dense
-                  height="300"
-                  :headers="position_headers"
-                  :items="positions"
-                  item-key="id"
-                  fixed-header
-                  disable-pagination
-                  hide-default-footer
-                ></v-data-table>
-              </v-tab-item>
+                <v-tab-item transition="none" reverse-transition="none">
+                  <v-data-table
+                    dense
+                    height="300"
+                    :headers="position_headers"
+                    :items="positions"
+                    item-key="id"
+                    fixed-header
+                    disable-pagination
+                    hide-default-footer
+                  ></v-data-table>
+                </v-tab-item>
 
-              <v-tab-item transition="none" reverse-transition="none">
-                <v-data-table dense :headers="order_headers" :items="orders" item-key="id">
-                  <template v-slot:item.state="{ item }">
-                    <v-chip color="green" dark label x-small>{{ item.state }}</v-chip>
-                    <!--                 <div class="green text-center">
+                <v-tab-item transition="none" reverse-transition="none">
+                  <v-data-table dense :headers="order_headers" :items="orders" item-key="id">
+                    <template v-slot:item.state="{ item }">
+                      <v-chip color="green" dark label x-small>{{ item.state }}</v-chip>
+                      <!--                 <div class="green text-center">
                   <span class="white--text">{{ item.state }}</span>
                 </div>
-                    -->
-                  </template>
-                </v-data-table>
-              </v-tab-item>
+                      -->
+                    </template>
+                  </v-data-table>
+                </v-tab-item>
 
-              <v-tab-item transition="none" reverse-transition="none">
-                <v-data-table dense :headers="position_headers" :items="positions" item-key="id"></v-data-table>
-              </v-tab-item>
-            </v-tabs>
+                <v-tab-item transition="none" reverse-transition="none">
+                  <v-data-table dense :headers="position_headers" :items="positions" item-key="id"></v-data-table>
+                </v-tab-item>
+              </v-tabs>
+            </v-card>
           </v-col>
         </v-row>
-      </v-col>
-
-      <v-col cols="2">
-        <v-navigation-drawer absolute permanent right>
-          <v-form ref="form" lazy-validation>
-            <v-container>
-              <v-row dense>
-                <v-col>
-                  <v-text-field dense disabled label="Bid" outline />
-                </v-col>
-                <v-col>
-                  <v-text-field dense disabled label="Ask" outline />
-                </v-col>
-              </v-row>
-
-              <v-row dense>
-                <v-col>
-                  <v-select dense :items="['Limit','Market']" value="Limit"></v-select>
-                </v-col>
-                <v-col>
-                  <v-select dense :items="['Day','GTC']" value="Day"></v-select>
-                </v-col>
-              </v-row>
-
-              <v-row dense>
-                <v-col>
-                  <v-text-field dense v-model="amount" label="Amount" required></v-text-field>
-                </v-col>
-                <v-col>
-                  <v-text-field dense v-model="price" label="Price" required></v-text-field>
-                </v-col>
-              </v-row>
-
-              <v-row no-gutters>
-                <v-col>
-                  <v-btn small @click="genSeries" block color="success">Buy</v-btn>
-                </v-col>
-                <v-col>
-                  <v-btn small @click="handlerSell" block color="error">Sell</v-btn>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-form>
-        </v-navigation-drawer>
       </v-col>
     </v-row>
   </v-container>
@@ -164,8 +126,7 @@ export default {
     };
   },
   watch: {
-    'seriesOhlc': function (newVal) {
-      console.log(newVal)
+    seriesOhlc: function(newVal) {
       this.stockOptions.series = newVal;
     }
   },
@@ -183,10 +144,21 @@ export default {
       genSeries: "terminal/series"
     })
   },
-
   created() {},
   mounted() {
     this.stockOptions.series = this.seriesOhlc;
   }
 };
 </script>
+
+<style scoped lang="css">
+.terminal-layout {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  grid-template-areas: "dashboard-drawer";
+  background-color: white;
+  padding: 0px;
+  margin: 0px;
+}
+</style>
