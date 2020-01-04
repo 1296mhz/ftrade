326a4f9d5f21c258b5e01e79ee60c0c47b1fd219
Ilia Atanov <chip0@mail.ru>
2020-01-04 10:18:07 +0500

    Прототип страницы

--- src/views/Templates.vue
+++ src/views/Templates.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-      <v-navigation-drawer v-model="drawer" permanent absolute>
+  <v-container>
+    <v-row>
+  
+<!--       <v-navigation-drawer v-model="drawer" permanent absolute>
         <v-list-item>
           <v-list-item-avatar>
             <v-icon>extension</v-icon>
@@ -13,23 +15,80 @@
 
         <v-divider></v-divider>
         <v-list dense>
-          <v-list-item v-for="item in items" :key="item.title" link>
+
+        <v-list-group v-for="cat in categories" :key="cat.id" no-action sub-group>
+
+          <template v-slot:prepend-icon>
+              folder
+          </template>
+
+
+          <template v-slot:activator>
+             <v-list-item-icon>
+              <v-icon>folder</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ cat.name }}</v-list-item-title>
+          </template>
+
+          <v-list-item v-for="cat in categories" :key="cat.id" link>
             <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
+              <v-list-item-title>{{ cat.name }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
+
+        </v-list-group>
+
         </v-list>
       </v-navigation-drawer>
-
-   
-
-      <v-container fluid fill-height>
+ -->    
+      <v-col>
         <v-card>
-          <v-text-field v-model="currentTemplate.script"></v-text-field>
-        </v-card>
-      </v-container>
+          <v-treeview
+            v-model="tree"
+            :open="open"
+            :items="items"
+            activatable
+            item-key="name"
+            transition="true"
+            dense
+          >
 
- </div>
+            <template v-slot:prepend="{ item, open }">
+              <v-icon v-if="!item.file">
+                {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
+              </v-icon>
+              <v-icon v-else>
+                {{ files[item.file] }}
+              </v-icon>
+            </template>
+
+            <template v-slot:label="{ item }">
+              <v-hover v-slot:default="{ hover }">
+                <div>
+              {{ item.name }}
+                <v-icon v-if="hover" small>cancel</v-icon>
+                </div>
+              </v-hover>
+            </template>
+
+            <template v-slot:append="{ active }">
+                <v-icon v-if="active" small>cancel</v-icon>
+            </template>
+
+
+
+          </v-treeview>
+        </v-card>
+      </v-col>
+
+      <v-col>
+          <v-card>
+            <v-text-field v-model="currentTemplate.script"></v-text-field>
+          </v-card>
+      </v-col>
+  
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -46,11 +105,77 @@ export default Vue.extend({
   data() {
     return {
       drawer: true,
+
       items: [
-        { title: 'Home', icon: 'mdi-home-city' },
-        { title: 'My Account', icon: 'mdi-account' },
-        { title: 'Users', icon: 'mdi-account-group-outline' },
+        {
+          name: '.git',
+        },
+        {
+          name: 'node_modules',
+        },
+        {
+          name: 'public',
+          children: [
+            {
+              name: 'static',
+              children: [{
+                name: 'logo.png',
+                file: 'png',
+              }],
+            },
+            {
+              name: 'favicon.ico',
+              file: 'png',
+            },
+            {
+              name: 'index.html',
+              file: 'html',
+            },
+          ],
+        },
+        {
+          name: '.gitignore',
+          file: 'txt',
+        },
+        {
+          name: 'babel.config.js',
+          file: 'js',
+        },
+        {
+          name: 'package.json',
+          file: 'json',
+        },
+        {
+          name: 'README.md',
+          file: 'md',
+        },
+        {
+          name: 'vue.config.js',
+          file: 'js',
+        },
+        {
+          name: 'yarn.lock',
+          file: 'txt',
+        },
       ],
+      files: {
+        html: 'mdi-language-html5',
+        js: 'mdi-nodejs',
+        json: 'mdi-json',
+        md: 'mdi-markdown',
+        pdf: 'mdi-file-pdf',
+        png: 'mdi-file-image',
+        txt: 'mdi-file-document-outline',
+        xls: 'mdi-file-excel',
+      },
+
+
+      categories: [
+        { id: 'cid1', name: 'Category 1'},
+        { id: 'cid2', name: 'Category 2'},
+        { id: 'cid3', name: 'Category 3'},
+      ],
+
       mini: true,
       templates: [],
       currentTemplate: {
